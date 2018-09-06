ddc64b8ee106d70354741fb957a478f07fb126a1
Daniel Segovia <dsegovia@leftfieldlabs.com>
2018-09-06 12:13:05 -0500

    publish a json file after scrapping

--- source/scrapers/gungeoneers/index.js
+++ source/scrapers/gungeoneers/index.js
@@ -4,14 +4,21 @@
  */
 
 import path from 'path';
+import fs from 'fs';
 import getHtmlFile from '../utils/getHtmlFile';
 import parseGungeoneers from './parseGungeoneers';
 
 const fileName = 'gungeoneers.html';
 const fileUrl = path.join(__dirname, fileName);
 const wikiUrl = 'https://enterthegungeon.gamepedia.com/Gungeoneers';
+const gunDataUrl = path.join(__dirname, 'gungeoneers.json');
 
 getHtmlFile(fileName, fileUrl, wikiUrl).then((html) => {
   // @TODO do something with this json object of the gungeoneers
-  parseGungeoneers(html);
+  const parsedGungeoneers = JSON.stringify(parseGungeoneers(html));
+  fs.writeFile(gunDataUrl, parsedGungeoneers, 'utf8', ((err) => {
+    if (err) {
+      console.error(err);
+    }
+  }));
 });
--- source/scrapers/guns/index.js
+++ source/scrapers/guns/index.js
@@ -4,14 +4,20 @@
  */
 
 import path from 'path';
+import fs from 'fs';
 import getHtmlFile from '../utils/getHtmlFile';
 import parseGuns from './parseGuns';
 
 const fileName = 'guns.html';
 const fileUrl = path.join(__dirname, fileName);
 const wikiUrl = 'https://enterthegungeon.gamepedia.com/Guns';
+const gunDataUrl = path.join(__dirname, 'guns.json');
 
 getHtmlFile(fileName, fileUrl, wikiUrl).then((html) => {
-  const parsedGuns = parseGuns(html);
-
+  const parsedGuns = JSON.stringify(parseGuns(html));
+  fs.writeFile(gunDataUrl, parsedGuns, 'utf8', ((err) => {
+    if (err) {
+      console.error(err);
+    }
+  }));
 });
